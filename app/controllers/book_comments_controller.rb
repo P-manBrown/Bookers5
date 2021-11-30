1bd681b329f688e36de2694ef1c3d52d61a5a542
P-manBrown <tachikawa.yuya@outlook.jp>
2021-11-30 16:46:25 +0000

    [finish]2-1-3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,17 @@
 class BookCommentsController < ApplicationController
-    
+
     def create
-        book = Book.find(params[:book_id])
-        comment = current_user.book_comments.new(book_comment_params)
-        comment.book_id = book.id
-        comment.save
-        redirect_back(fallback_location: books_path)
+        @book = Book.find(params[:book_id])
+        @comment = current_user.book_comments.new(book_comment_params)
+        @comment.book_id = @book.id
+        if @comment.save
+  		  redirect_to book_path(@book.id)
+  		else
+		  @user = @book.user
+		  @book_new = Book.new
+		  @book_comment = BookComment.new
+		  render  'books/show'
+		end
     end
 
     def destroy
